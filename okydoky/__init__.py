1e4f39c04eb8c099a19ac50c35a0122ab6ed262c
Hong Minhee <dahlia@crosspop.in>
2012-07-16 18:53:55 +0900

    Empty page

--- okydoky/__init__.py
+++ okydoky/__init__.py
@@ -13,12 +13,16 @@ from werkzeug.urls import url_decode, url_encode
 app = Flask(__name__)
 
 
-def open_token_file(mode='r', config=None):
+def open_file(filename, mode='r', config=None):
     config = config or current_app.config
     save_path = config['SAVE_DIRECTORY']
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
-    return open(os.path.join(save_path, 'token.txt'), mode)
+    return open(os.path.join(save_path, filename), mode)
+
+
+def open_token_file(mode='r', config=None):
+    return open_file('token.txt', mode)
 
 
 def get_token(config=None):
@@ -35,12 +39,28 @@ def get_token(config=None):
     return token
 
 
+def open_head_file(mode='r', config=None):
+    return open_file('head.txt', mode)
+
+
+def get_head(config=None):
+    try:
+        with open_head_file(config=config) as f:
+            return f.read().strip()
+    except IOError:
+        pass
+
+
 @app.route('/')
 def home():
     token = get_token()
-    if token is not None:
-        return token
-    return render_template('home.html', login_url=url_for('auth_redirect'))
+    if token is None:
+        return render_template('home.html', login_url=url_for('auth_redirect'))
+    head = get_head()
+    if head is None:
+        hook_url = url_for('post_receive_hook', _external=True)
+        return render_template('empty.html', hook_url=hook_url)
+    return token + '\n' + token
 
 
 @app.route('/auth/1')
